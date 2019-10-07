650247a49172cc63ae859b77c681e8c383230061
Kage0x3B <kage0x3b@outlook.com>
2019-10-07 21:10:36 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+require('./styles/index.scss');
+
+import 'popper.js';
+import jquery from 'jquery';
+
+import './sidebar';
---
+++ src/sidebar.js
@@ -0,0 +1,16 @@
+import jquery from 'jquery';
+const $ = jquery;
+
+$(document).ready(function () {
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').addClass('active');
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});

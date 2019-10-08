589feaa82f0e6efcc66919fb80bb0d373a774579
Moritz Hein <kage0x3b@outlook.com>
2019-10-08 04:07:31 +0200

    Almost finished home section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-require('./styles/index.scss');
+import './styles/index.scss';
 
 import 'popper.js';
-import jquery from 'jquery';
+import jQuery from 'jquery';
+import '@fortawesome/fontawesome-free/js/all';
 
 import './sidebar';
+
+jQuery(function () {
+    const overlayElem = jQuery("#loadOverlay");
+    overlayElem.addClass("hide");
+
+    setTimeout(function () {
+        overlayElem.removeClass("hide");
+        overlayElem.addClass("hidden");
+        overlayElem.attr("hidden", true);
+        jQuery("html").removeClass("loading");
+    }, 700);
+});

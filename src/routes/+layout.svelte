98def4e64b417c9cfe22569b1f996853d2ba609e
Moritz Hein <moritz.hein@live.de>
2025-01-03 01:43:00 +0100

    Migration to Svelte v5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+    interface Props {
+        children?: import('svelte').Snippet;
+    }
+
+    let { children }: Props = $props();
 </script>
 
 <svelte:head>
     <title>Moritz Hein</title>
 </svelte:head>
 
-<slot />
+{@render children?.()}
--- src/routes/old/+layout.svelte
+++ src/routes/old/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
     import '$lib/css/main.scss';
+    interface Props {
+        children?: import('svelte').Snippet;
+    }
+
+    let { children }: Props = $props();
 </script>
 
 <svelte:head>
     <title>Moritz Hein</title>
 </svelte:head>
 
-<slot />
+{@render children?.()}
